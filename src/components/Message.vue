6de35e819bd1fb561b292204b3bf4629a5df2e43
Pereirapam <pereirapamela690@gmail.com>
2025-08-07 14:25:55 -0300

    Último ajuste

---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="message-container">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+    .message-container {
+        color: #004085;
+        background-color: #cce5ff;
+        border-color: #b8daff;
+        padding: 10px;
+        border-radius: 5px;
+        margin: 30px;
+        max-width: 400px;
+        align-items: center;
+    }
+</style>
